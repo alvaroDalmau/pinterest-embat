686a89af608f686d4a962899cdb389f91edd537a
alvaroDalmau <dalmau.alvaro@gmail.com>
2021-10-18 17:01:42 +0200

    Updates

---
+++ precache-manifest.c4aaf3ec85fb3e7dbd5d0de07dd7d685.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a49aa6a31342bb7bfe8af351233822b5",
+    "url": "/pinterest-embat/index.html"
+  },
+  {
+    "revision": "5026c6a0f8440163e47f",
+    "url": "/pinterest-embat/static/css/2.68a81a89.chunk.css"
+  },
+  {
+    "revision": "c6ddc548d4255c731d59",
+    "url": "/pinterest-embat/static/css/main.d74a205d.chunk.css"
+  },
+  {
+    "revision": "5026c6a0f8440163e47f",
+    "url": "/pinterest-embat/static/js/2.84fd1e2d.chunk.js"
+  },
+  {
+    "revision": "522e03d803bfdb0e0cda4e24f58dcf37",
+    "url": "/pinterest-embat/static/js/2.84fd1e2d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "874558b648e55c3ec8de",
+    "url": "/pinterest-embat/static/js/3.3712566a.chunk.js"
+  },
+  {
+    "revision": "c6ddc548d4255c731d59",
+    "url": "/pinterest-embat/static/js/main.d98b7e4f.chunk.js"
+  },
+  {
+    "revision": "9f2bff2180020cdc0c7f",
+    "url": "/pinterest-embat/static/js/runtime-main.73a07dce.js"
+  },
+  {
+    "revision": "899cd5e1bc97ea4c94c0b088b0fa23d8",
+    "url": "/pinterest-embat/static/media/logo.899cd5e1.png"
+  }
+]);
